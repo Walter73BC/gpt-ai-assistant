ed7bc88c8dcc0b61d677f3896a52464fb3688ecd
WalChain <95265968+Walter73BC@users.noreply.github.com>
2023-01-03 19:09:43 +0800

    Update openai.js

--- services/openai.js
+++ services/openai.js
@@ -31,16 +31,16 @@ const createCompletion = ({
   maxTokens = config.OPENAI_COMPLETION_MAX_TOKENS,
   frequencyPenalty = config.OPENAI_COMPLETION_FREQUENCY_PENALTY,
   presencePenalty = config.OPENAI_COMPLETION_PRESENCE_PENALTY,
-  stop: [
-    "User:",
-    "CCBot:",
+  stop = [
+    ` ${PARTICIPANT_AI}:`,
+    ` ${PARTICIPANT_HUMAN}:`,
   ],
 }) => instance.post('/v1/completions', {
   model,
   prompt,
   temperature,
   max_tokens: maxTokens,
-  top_p: top_p
+  top_p: top_p,
   frequency_penalty: frequencyPenalty,
   presence_penalty: presencePenalty,
   stop,
